7d6e37921cdff710456f45948d2247957957ef72
S1ngularty <asherxd10245@gmail.com>
2026-01-05 19:29:42 +0800

    login screen style

--- todoList-App/src/screens/loginScreen.js
+++ todoList-App/src/screens/loginScreen.js
@@ -17,30 +17,97 @@ export default function LoginScreen() {
   });
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <View>
-          <Text>Username:</Text>
+    <SafeAreaView style={style.main}>
+      <View style={style.container}>
+        <View style={style.inputView}>
+          <Text style={style.inputLabel}>Username:</Text>
           <TextInput
+            style={style.inputBox}
             onChangeText={(text) =>
               setCredentials((prev) => ({ ...prev, username: text }))
             }
             value={credentials.username}></TextInput>
         </View>
-        <View>
-          <Text>Password:</Text>
+        <View style={style.inputView}>
+          <Text style={style.inputLabel}>Password:</Text>
           <TextInput
+            style={style.inputBox}
             onChangeText={(text) =>
               setCredentials((prev) => ({ ...prev, password: text }))
             }
             value={credentials.password}></TextInput>
         </View>
-        <View>
-          <Pressable>
-            <Text>Login</Text>
+        <View style={style.buttonView}>
+          <Pressable style={style.button}>
+            <Text style={style.buttonText}>Login</Text>
           </Pressable>
         </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const style = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: "start",
+    alignItems: "start",
+    paddingHorizontal: "30%",
+  },
+  container: {
+    backgroundColor: "#eae6e6ff",
+    height: "50%",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderRadius: 17,
+    borderColor: "#ccc",
+  },
+  inputView: {
+    width: "89%",
+    marginVertical: "3%",
+    marginHorizontal: "5%",
+    paddingBottom: "5%",
+  },
+
+  inputLabel: {
+    fontSize: 16,
+    fontWeight: "600",
+  },
+
+  inputBox: {
+    marginTop: 12,
+    height: 50,
+    color: "#000",
+    fontSize: 16,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#ccc",
+    backgroundColor: "#fff",
+    paddingHorizontal: 12,
+  },
+
+  buttonView: {
+    width: "100%",
+    alignItems: "center",
+    height: 50,
+    paddingHorizontal: 20,
+  },
+  button: {
+    height:50,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#007AFF",
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#007AFF",
+    color: "#fff",
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#fff",
+  },
+});
